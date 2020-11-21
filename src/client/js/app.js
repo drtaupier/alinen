06d440f8bc6d05c3cb7d6d71e6d52f8a9138ab12
David Rivera <drtaupier@gmail.com>
2020-11-20 21:35:04 -0500

    I set a error msg

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -5,10 +5,10 @@ window.onload = function(){
     ui.footer();
 
     const formulario = document.querySelector('form');
-    formulario.addEventListener('click', e => {
+    formulario.addEventListener('submit', e => {
         e.preventDefault();
-        console.log('Hola');
-        validaForm();
+        ui.deleteMsg();
+        validaForm(); 
     });
 
     function validaForm(){
@@ -16,13 +16,39 @@ window.onload = function(){
         const psw = document.querySelector('#psw').value.trim();
         const day = document.querySelector('select').value;
 
-        if(user === ''){
-            console.log('Usuario esta vacio');
+        if(user === '' && psw === ''){
+            ui.msgError("Por favor, ingrese sus credenciales");
+        }else if(user === ''){
+            ui.msgError("Por favor, ingrese el usuario");
             return false;
-        }
-        if(psw === ''){
-            console.log('Ingrese su password');
+        }else if(psw === ''){
+            ui.msgError('Por favor, ingrese su contraseña');
             return false;
+        }else if(day == 0){
+            ui.msgError("Por favor, seleccione el día que le corresponde");
+            return false;
+        }else{
+            console.log('I did login');
+            postData('/mylogin', {'user': user, 'psw': psw, 'day': day, 'fecha': fecha });
         }
+    }
+}
+
+const postData = async(url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST', //*GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        //Body data type must match "Content-Type" header
+        body: JSON.stringify(data),
+    });
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    } catch (error) {
+        console.log('Error: ', error);
     }
 }
--- src/client/js/classes.js
+++ src/client/js/classes.js
@@ -5,4 +5,36 @@ export class UI{
         const year = today.getFullYear();
         footer.innerHTML = `<h3>Copyright &copy ${year}</h3>`;
     }
+
+    msgError(mensaje){
+        const section = document.querySelector('section');
+        const element = document.createElement('div');
+        element.setAttribute('id', 'errorMsg');
+        element.innerHTML = `<h2>${mensaje}</h2>`;
+        section.appendChild(element);
+        setTimeout( ()=> { document.querySelector('#errorMsg').remove() }, 3000 );
+    }
+
+    deleteMsg(){
+        //Este metodo es para que el mensaje no se pueda repetir al presionar varias veces Ingresar.
+        const send = document.querySelector('#enviar');
+        send.addEventListener('click', ()=>{
+            const mensaje = send.parentElement.parentElement.nextElementSibling;
+            if(mensaje){
+                mensaje.remove();
+            }
+        })
+    }
+
+    cleanForm(){
+        const form = document.querySelector('form');
+        form.reset();
+    }
+
+    fechaActual(){
+        const today = new Date();
+        const hour = today.getUTCHours();
+        const minutes = today.getMinutes();
+        const seconds = today.getUTCSeconds()        
+    }
 }
