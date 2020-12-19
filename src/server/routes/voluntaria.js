d67056a5d0ba25fddaa9ae4f37bcf725eda3f72f
David Rivera <drtaupier@gmail.com>
2020-12-19 14:05:07 -0500

    Se genero una busqueda en la DB recibiendo argumento por la URL

--- src/server/routes/voluntaria.js
+++ src/server/routes/voluntaria.js
@@ -40,33 +40,38 @@ app.get("/voluntarias", verificaToken, (req, res) => {
 //Muestra una voluntaria por ID
 app.get("/voluntarias/:id", verificaToken, (req, res) => {
 	let id = req.params.id;
-	Voluntaria.findById(id, (err, voluntariaDB) => {
-		if (err) {
-			res.status(500).json({
-				ok: false,
-				err,
+	Voluntaria.findById(id)
+		.populate("usuario", "nombre apellido")
+		.populate("dia", "dia")
+		.exec((err, voluntariaDB) => {
+			if (err) {
+				res.status(500).json({
+					ok: false,
+					err,
+				});
+			}
+			if (!voluntariaDB) {
+				res.status(500).json({
+					ok: false,
+					err: {
+						message: "El ID no es correcto",
+					},
+				});
+			}
+			res.json({
+				ok: true,
+				voluntaria: voluntariaDB,
 			});
-		}
-		if (!voluntariaDB) {
-			res.status(500).json({
-				ok: false,
-				err: {
-					message: "El ID no es correcto",
-				},
-			});
-		}
-		res.json({
-			ok: true,
-			voluntaria: voluntariaDB,
 		});
-	});
 });
 
 //Buscar voluntaria:
 app.get("/voluntarias/buscar/:termino", verificaToken, (req, res) => {
 	let termino = req.params.termino;
-	Voluntaria.find({ nombre: termino })
-		.populate("dias", "dia") //primero es el nombre de la colección
+	let regex = new RegExp(termino, "i"); //este Object se usa para expresiones regulares y con la i le indicamos que sea insensible a mayusculas y minusculas
+	Voluntaria.find({ email: regex })
+		.populate("usuario", "nombre apellido")
+		.populate("dia", "dia") //el primer campo es el nombre de la propiedad que se indico en el Schema
 		.exec((err, voluntarias) => {
 			if (err) {
 				res.status(500).json({
