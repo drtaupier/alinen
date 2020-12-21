c9b66fb6920977bdeb1dbe06048b73ba7080ef75
David Rivera <drtaupier@gmail.com>
2020-12-20 20:03:57 -0500

    Se agrego el middleware upload

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(require('./usuario'));
-app.use(require('./login'));
-app.use(require('./voluntaria'));
-app.use(require('./dias'));
+app.use(require("./usuario"));
+app.use(require("./login"));
+app.use(require("./voluntaria"));
+app.use(require("./dias"));
+app.use(require("./upload"));
 
 module.exports = app;
